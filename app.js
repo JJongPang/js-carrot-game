3a25aa23860c256279fafa510bae6e27cb782d18
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-08-25 05:36:40 +0900

    JS showStopButton, showTimerAndScore

--- app.js
+++ app.js
@@ -1,19 +1,69 @@
+'use strict'
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
 
+let started = false;
+let score = 0;
+let timer = undefined;
+
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
+//start button 'click'
+gameBtn.addEventListener('click', () => {
+    if(started) {
+        stopGame();
+    }else {
+        startGame();
+    }
+    started = !started;
+});
+
+//start game
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGmaeTimer();
+}
+
+//stop game
+function stopGame() {
+
+}
+
+//change stop button
+function showStopButton() {
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+//view : timer, score style setting
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
 
 //item init field
 function initGame() {
-    addItem('carrot', 5, 'img/carrot.png');
-    addItem('bug', 5, 'img/bug.png');
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT; 
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', BUG_COUNT, 'img/bug.png');
 }
 
 //Random add Item
 function addItem(className, count, imgPath) {
     const x1 = 0;
     const y1 = 0;
-    const x2 = fieldRect.width;
-    const y2 = fieldRect.height;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
     for(let i = 0; i < count; i++) {
         const item = document.createElement('img');
         item.setAttribute('class', className);
@@ -32,4 +82,3 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-initGame();
